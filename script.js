074a4a02b75c30624c4c10159d57f0efde947907
justinbrick <justinbrick1@gmail.com>
2022-05-01 21:26:31 -0400

    Update contact information

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
 window.addEventListener("load", function() {
-    console.log("Body creation");
     let div = document.createElement("div");
     document.body.appendChild(div);
     for (var i = 0; i < 100; ++i) {
         var star = document.createElement("div");
         $(star).addClass("star");
-        star.style.left = Math.floor(Math.random() * 100) + "vw";
-        star.style.top = Math.floor(Math.random() * 100) + "vh";
+        star.style.left = Math.floor(Math.random() * 100) + "%";
+        star.style.top = Math.floor(Math.random() * 100) + "%";
         star.style.animation = (Math.random()*2 + 0.5) + "s ease 1s alternate infinite fade";
         div.appendChild(star);
     }
