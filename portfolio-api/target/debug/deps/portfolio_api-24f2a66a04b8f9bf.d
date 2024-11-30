4e5ef2e94926d28175da26eb27435aac33a6c055
Justin <justin@justin.directory>
2024-11-29 22:30:09 -0500

    remove errant target dir

--- portfolio-api/target/debug/deps/portfolio_api-24f2a66a04b8f9bf.d
+++
@@ -1,5 +0,0 @@
-/home/justin/repos/justin.directory/portfolio-api/target/debug/deps/libportfolio_api-24f2a66a04b8f9bf.rmeta: src/main.rs
-
-/home/justin/repos/justin.directory/portfolio-api/target/debug/deps/portfolio_api-24f2a66a04b8f9bf.d: src/main.rs
-
-src/main.rs:
