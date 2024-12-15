fd318e1712cb4cfb2d25f990b4e365749d3f6ec8
Justin <justin@justin.directory>
2024-12-14 22:10:31 -0500

    fix(website): change link to /contact

--- website/components/NavBar.vue
+++ website/components/NavBar.vue
@@ -4,7 +4,7 @@
 <template>
     <nav>
         <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
     </nav>
 </template>
 
